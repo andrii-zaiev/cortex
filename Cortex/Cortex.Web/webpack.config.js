74b2e2118f85afec12b1e9e32f05176acdca4077
Andrii Zaiev <andrii.zaiev@gmail.com>
2018-03-03 18:00:58 +0200

    Main page UI was finished

--- Cortex/Cortex.Web/webpack.config.js
+++ Cortex/Cortex.Web/webpack.config.js
@@ -20,11 +20,26 @@ module.exports = {
             },
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
+                use: [
+                    {
+                        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+                        options: {
+                            outputPath: '../fonts/',
+                            publicPath: './dist/.'
+                        }
+                    }
+                ]
             },
             {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: '../fonts/'
+                        }
+                    }
+                ]
             }
         ]
     },
