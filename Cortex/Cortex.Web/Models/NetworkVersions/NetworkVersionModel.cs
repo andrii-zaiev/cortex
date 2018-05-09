ac4dd68527dd8f1fa7d1d98adf4a7c9bf1287c2f
Andrii Zaiev <andrii.zaiev@gmail.com>
2018-05-09 18:33:28 +0300

    Basic versions list was created

---
+++ Cortex/Cortex.Web/Models/NetworkVersions/NetworkVersionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cortex.Web.Models.Shared;
+
+namespace Cortex.Web.Models.NetworkVersions
+{
+    public class NetworkVersionModel
+    {
+        public NetworkVersionModel()
+        {
+            
+        }
+
+        public Guid Id { get; }
+
+        public string Comment { get; }
+
+        public DateTimeOffset CreatedDate { get; }
+
+        public UserDisplayModel Author { get; }
+    }
+}
---
+++ Cortex/Cortex.Web/ViewComponents/NetworkVersionsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Cortex.Web.Models.NetworkVersions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cortex.Web.ViewComponents
+{
+    public class NetworkVersionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Guid networkId)
+        {
+            return View(new List<NetworkVersionModel>());
+        }
+    }
+}
