0e4545410e5969fe00a21b0d88455107d9e73556
Andrii Zaiev <andrii.zaiev@gmail.com>
2018-05-26 14:42:18 +0300

    API enpoint for users searching was added

---
+++ Cortex/Cortex.Web/Controllers/Api/UsersApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cortex.Auth;
+using Cortex.Services.Dtos;
+using Cortex.Services.Interfaces;
+using Cortex.Web.Models.Api;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cortex.Web.Controllers.Api
+{
+    [Authorize]
+    public class UsersApiController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersApiController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("api/users")]
+        public async Task<IActionResult> FindUsers(string query)
+        {
+            if (String.IsNullOrEmpty(query) || query.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            IList<User> users = await _userService.FindUsersAsync(query);
+
+            List<UserModel> result = users
+                .Where(u => u.Id != User.GetId())
+                .Select(u => new UserModel(u))
+                .ToList();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Cortex/Cortex.Web/Models/Api/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cortex.Services.Dtos;
+
+namespace Cortex.Web.Models.Api
+{
+    public class UserModel
+    {
+        public UserModel(User user)
+        {
+            Id = user.Id;
+            UserName = user.UserName.ToLower();
+            Name = user.Name;
+        }
+
+        public Guid Id { get; }
+
+        public string UserName { get; }
+
+        public string Name { get; }
+    }
+}
