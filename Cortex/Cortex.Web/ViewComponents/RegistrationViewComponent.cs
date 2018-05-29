55000db1be3e2f0728c54f645276fadd07aa7dc0
Andrii Zaiev <andrii.zaiev@gmail.com>
2018-05-30 01:06:07 +0300

    Top part of the landing page was created

---
+++ Cortex/Cortex.Web/ViewComponents/RegistrationViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cortex.Web.ViewComponents
+{
+    public class RegistrationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
