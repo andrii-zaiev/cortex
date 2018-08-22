8edf69e69a35b2609faf6671c182e1fd4e7d6561
Andrii Zaiev <andrii.zaiev@gmail.com>
2018-08-22 16:29:17 +0300

    AGPL license headers were added

--- Cortex/Cortex.Web/Source/scripts/network-editor/components/NetworkDisplayArea.tsx
+++ Cortex/Cortex.Web/Source/scripts/network-editor/components/NetworkDisplayArea.tsx
@@ -1,4 +1,21 @@
-ï»¿import * as React from 'react';
+// Cortex - web application for creating and managing neural networks diagrams.
+// Copyright (C) 2018  Andrii Zaiev <andrii.zaiev@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+import * as React from 'react';
 import * as d3 from 'd3';
 
 import { Record, Map, List } from 'immutable';
@@ -124,7 +141,7 @@ export default class NetworkDisplayArea
         const layerInfoLabel = this.updateInfoLabels(layerLabel.select('tspan#info'));
         const layerSizeLabel = this.updateSizeLabels(layerLabel.select('tspan#size'));
 
-        // Enterâ€¦
+        // Enter…
         const groupEnter = layerGroup.enter().append('g');
         this.updateLayerRects(groupEnter.append('rect')
             .attr('id', 'front')
@@ -153,7 +170,7 @@ export default class NetworkDisplayArea
         this.updateInfoLabels(layerEnter.append('tspan').attr('id', 'info').attr('dy', '1.2em'));
         this.updateSizeLabels(layerEnter.append('tspan').attr('id', 'size').attr('dy', '1.2em'));
 
-        // Exitâ€¦
+        // Exit…
         layerGroup.exit().remove();
     }
 
